70dadf3a5057b51346c39d67ecd772e1883402be
Wyatt Smith <ws998116@gmail.com>
2025-03-29 11:38:17 -0400

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from 'next'
 import { WEBSITE_URL } from '@/lib/constants'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
