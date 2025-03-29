a54a504f635e05ceb20f2165f054ae655ca93f2f
Wyatt Smith <ws998116@gmail.com>
2025-03-29 11:33:39 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
