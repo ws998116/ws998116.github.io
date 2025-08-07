88553cd68f777013d79b13714f81efc91d734e78
Wyatt Smith <ws998116@gmail.com>
2025-08-07 08:30:42 -0400

    remove Copy URL button

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -43,9 +43,9 @@ export default function LayoutBlogPost({
         }}
       />
 
-      <div className="absolute right-4 top-24">
+      {/* <div className="absolute right-4 top-24">
         <CopyButton />
-      </div>
+      </div> */}
       <main className="prose prose-gray mt-24 pb-20 prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium">
         {children}
       </main>
